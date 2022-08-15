ac727280aaf5a2cb707e5049dcbbd24a64c40b75
Saketh Reddy <45369057+SpiritSeal@users.noreply.github.com>
2022-08-15 13:52:03 -0700

    Post Monday

--- modules/generateGraphs.py
+++ modules/generateGraphs.py
@@ -11,6 +11,7 @@ def objective(x, a, b, p):
     # return a*e**(p*x) + b
     # a*e**(p*x) + b
     # I = a*(e**(p*x)-1)
+    # (1/(25.852*10**-3))*
     return a*(e**(p*x)-1) + b
 
 
@@ -29,17 +30,29 @@ def generateIndivGraph(colorVal):
     plt.scatter(colorDf['Vdio'], colorDf['I'], c=colorVal, cmap=plt.cm.Blues, edgecolors='none')
     # Create a regression curve using the data and the objective function.
     # param_bounds=([-np.inf,-np.inf,0],[np.inf,np.inf,np.inf])
+    # param_bounds=([-20,-np.inf,-np.inf],[20,np.inf,np.inf])
     param_bounds=([-np.inf,-np.inf,-np.inf],[np.inf,np.inf,np.inf])
     popt, pcov = curve_fit(objective, colorDf['Vdio'], colorDf['I'], maxfev=10000, bounds=param_bounds)
     # Print the parameters of the regression curve.
     print('For LED color "' + colorVal + '", the parameters are: [a =', popt[0], 'b =', popt[1], 'p =', popt[2], end=']\n')
     regression = 'For ' + colorVal + ' color, the parameters are: [a = '+ str(popt[0])+ ' b = ' + str(popt[1]), ' p = ' + str(popt[2]), ' ]'
     regressionResults.write(str(regression)+'\n')
-    # Plot the regression curve.
-    plt.plot(colorDf['Vdio'], objective(colorDf['Vdio'], *popt), 'r-', label=colorVal.capitalize()+ ' LED Diode Regression', color=colorVal, linewidth=.5)
+    # Plot the regression curve using the min and max values of the x-axis.
+    # Linspace the x-axis from the min and max values of the x-axis.
+    x = np.linspace(min(colorDf['Vdio']), max(colorDf['Vdio']), 100)
+
+
+    # plt.plot(colorDf['Vdio'], objective(colorDf['Vdio'], *popt), 'r-', label=colorVal.capitalize()+ ' LED Diode Regression', color=colorVal, linewidth=.5)
+    plt.plot(x, objective(x, *popt), 'r-', label=colorVal.capitalize()+ ' LED Diode Regression', color=colorVal, linewidth=.5)
     # Add a legend with white text.
     plt.legend(loc='upper left', facecolor='white', edgecolor='white', frameon=True)
     plt.savefig('outputs/' + colorVal + '.png')
+    plt.show()
+
+
+    
+    # Plot the original data and connect each point together no regression curve.
+    plt.plot(colorDf['Vdio'], colorDf['I'], color='white')
     plt.show()
 
 # List of colors to plot
